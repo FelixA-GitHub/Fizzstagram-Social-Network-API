5110ad2c69b65f9d6cd81c8aa9db5ce5ebc13a94
Your Name <felix.acevedo.tech@gmail.com>
2022-05-27 17:08:02 -0400

    thought route and index created

--- routes/api/thought-route.js
+++ routes/api/thought-route.js
@@ -3,6 +3,30 @@ const router = require('express').Router();
 const {
     getAllThoughts,
     getThoughtById,
-    createThought
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction
 } = require('../../controllers');
 
+// /api/thoughts
+router
+    .route('/')
+    .get(getAllThoughts)
+    .post(createThought);
+
+// /api/thoughts/:id
+router
+    .route('/:id')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router
+    .route('/:thoughtId/reactions')
+    .post(createReaction)
+    .delete(deleteReaction);
+
+module.exports = router;
