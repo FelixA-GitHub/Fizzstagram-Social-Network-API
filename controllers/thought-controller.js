6525c0f16357b4fae70e0108af9e213e162a74e9
Your Name <felix.acevedo.tech@gmail.com>
2022-05-28 18:23:16 -0400

    updating create thought and create reaction

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -22,11 +22,24 @@ const thoughtController = {
     },
 
     // createThought
-    createThought({ body }, res) {
+    createThought({ params, body }, res) {
         Thought.create(body)
-            .then(dbThoughtData => res.json(dbThoughtData))
-            .catch(err => res.json(err));
-    },
+        .then(({ _id }) => {
+            return User.findOneAndUpdate(
+              { _id: params.userId },
+              { $push: { thoughts: _id } },
+              { new: true }
+            );
+          })
+          .then(dbUserData => {
+            if (!dbUserData) {
+              res.status(404).json({ message: 'No User found with this id!' });
+              return;
+            }
+            res.json(dbUserData);
+          })
+          .catch(err => res.json(err));
+      },
 
     // update Thought by id
     updateThought({ params, body }, res) {
@@ -50,11 +63,17 @@ const thoughtController = {
             .catch(err => res.json(err));
     },
     // create reaction to thought
-    createReactionToThought({ params }, res) {
-        Thought.findOneAndUpdate({ _id: params.id, reactions: { $new: params.reactionId } }, { $push: { reactions: params.reactionId } }, { new: true, unique: true })
-            .then(dbThoughtData => res.json(dbThoughtData, { message: 'New reaction added!' }))
-            .catch(err => res.json(err));
-    },
+    createReactionToThought({ params, body }, res) {
+        Thought.findOneAndUpdate({ _id: params.thoughtId }, { $push: { reactions: body } }, { new: true, unique: true })
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+              res.status(404).json({ message: 'No Thought found with this id!' });
+              return;
+            }
+            res.json(dbThoughtData);
+          })
+          .catch(err => res.json(err));
+      },
 
     // delete reaction from thought
     deleteReactionToThought({ params }, res) {
